92798282138e23746235eca99dfa37f8004e0146
Andrew Black (RIT Student) <acb7086@rit.edu>
2024-11-08 13:43:43 -0500

    completion of domo maker tutorial b

--- server/router.js
+++ server/router.js
@@ -8,7 +8,8 @@ const router = (app) => {
   app.post('/signup', controllers.Account.signup);
 
   app.get('/logout', controllers.Account.logout);
-  app.get('/maker', controllers.Account.loginPage);
+  app.get('/maker', controllers.Domo.makerPage);
+  app.post('/maker', controllers.Domo.makeDomo);
   app.get('/', controllers.Account.loginPage);
 };
 
